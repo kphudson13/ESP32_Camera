7920dd933d1b932ac4c1c64ab5fd68720fff8e4b
kphudson13 <kphudson@live.ca>
2026-09-09 15:30:29 -0600

    Update gitignore

--- ESP32Cam/credentials.h
+++ ESP32Cam/credentials.h
@@ -2,8 +2,8 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
-const char* ssid = "The Continuum";
-const char* password = "8n1qnavs";
+const char* ssid = "hudnet14";
+const char* password = "4Family!";
 const char* smtp_email = "kph.arduino@gmail.com";
 const char* smtp_password = "fnbz vknr emgc zkye";
 const char* recip_email = "kphudson@live.ca";
