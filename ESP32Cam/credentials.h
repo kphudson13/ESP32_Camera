621da4d5a241a6f463f3cf49be02ca110f06e38a
Kyle Hudson <119540962+kphudson13@users.noreply.github.com>
2026-09-09 15:43:13 -0600

    Add credentials example

---
+++ ESP32Cam/credentials.h
@@ -0,0 +1,11 @@
+// credentials.h
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char* ssid = "your_wifi";
+const char* password = "your_password";
+const char* smtp_email = "sending_email";
+const char* smtp_password = "sending_email_password";
+const char* recip_email = "recieving_email";
+
+#endif
